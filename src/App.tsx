0d5d0868ae4ed3935742ea32d16ed2f0019ef7f8
diboti <korposb@gmail.com>
2025-04-03 20:38:43 +0200

    init homepage added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
-import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/Home";
+
+const router = createBrowserRouter([{ path: "/", element: <HomePage /> }]);
 
 function App() {
-  return <></>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
