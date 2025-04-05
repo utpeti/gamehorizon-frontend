b1d884075e419e156116257520481bab79798b80
diboti <korposb@gmail.com>
2025-04-06 00:31:29 +0200

    added login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
 
-const router = createBrowserRouter([{ path: "/", element: <HomePage /> }]);
+const router = createBrowserRouter([
+  { path: "/", element: <HomePage /> },
+  { path: "/login", element: <Login /> },
+]);
 
 function App() {
   return <RouterProvider router={router} />;
