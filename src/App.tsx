ac4530cd978f123d6a60cdbb375b53c53a2af60d
diboti <korposb@gmail.com>
2025-04-07 16:04:02 +0200

    added register page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
 
 const router = createBrowserRouter([
   { path: "/", element: <HomePage /> },
   { path: "/login", element: <Login /> },
+  { path: "/register", element: <Register /> },
 ]);
 
 function App() {
--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,13 +1,16 @@
 import { useRef, useState } from "react";
 import InputSection from "../../components/InputSection";
 
-function Login() {
+function Register() {
   const [userInputValue, setUserInputValue] = useState<string>("");
   const [passwordInputValue, setPasswordInputValue] = useState<string>("");
+  const [confirmPasswordInputValue, setConfirmPasswordInputValue] =
+    useState<string>("");
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
   const userInputRef = useRef<HTMLInputElement>(null);
   const passwordInputRef = useRef<HTMLInputElement>(null);
+  const confirmPasswordInputRef = useRef<HTMLInputElement>(null);
 
   async function submit(event: React.FormEvent<HTMLFormElement>) {
     setLoading(true);
@@ -47,30 +50,28 @@ function Login() {
   return (
     <div className="min-h-screen w-full bg-gradient-to-b from-indigo-900 via-stone-700 to-stone-900">
       <div className="flex flex-col items-center justify-center h-screen">
-        <h1 className="text-4xl font-bold text-[#F3E8EE] mb-8">Login</h1>
+        <h1 className="text-4xl font-bold text-[#F3E8EE] mb-8">Register</h1>
         <form
-          className="bg-white p-6 rounded-lg shadow-md w-96"
+          className="bg-white p-6 rounded-lg shadow-md w-1/2 h-2/5"
           onSubmit={submit}
         >
           <InputSection labelText="email" inputRef={userInputRef} />
           <InputSection labelText="password" inputRef={passwordInputRef} />
+          <InputSection
+            labelText="confirm password"
+            inputRef={confirmPasswordInputRef}
+          />
           <button
             type="submit"
             onClick={handleChange}
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mt-6 rounded focus:outline-none focus:shadow-outline w-full"
           >
-            Login
+            Register
           </button>
         </form>
-        <p className="mt-4 text-gray-600">
-          Don't have an account?{" "}
-          <a href="/register" className="text-blue-500 hover:text-blue-700">
-            Register here
-          </a>
-        </p>
       </div>
     </div>
   );
 }
 
-export default Login;
+export default Register;
