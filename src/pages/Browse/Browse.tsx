75fcc43ea03c6c7b082ad671361522d8ed58c45e
diboti <korposb@gmail.com>
2025-04-10 18:20:23 +0200

    initial browse page

--- src/pages/Browse/Browse.tsx
+++ src/pages/Browse/Browse.tsx
@@ -1,6 +1,10 @@
+import SearchField from "./SearchField";
+
 function Browse() {
   return (
-    <div className="min-h-screen w-full bg-gradient-to-b from-indigo-900 via-stone-700 to-stone-900"></div>
+    <div className="min-h-screen w-full bg-gradient-to-b from-indigo-900 via-stone-700 to-stone-900 pt-4">
+      <SearchField />
+    </div>
   );
 }
 
