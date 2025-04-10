ba5f1a5bbd25c8171f5298916fb022901da2049f
diboti <korposb@gmail.com>
2025-04-10 18:53:41 +0200

    designed browse input

--- src/pages/Browse/Browse.tsx
+++ src/pages/Browse/Browse.tsx
@@ -2,7 +2,7 @@ import SearchField from "./SearchField";
 
 function Browse() {
   return (
-    <div className="min-h-screen w-full bg-gradient-to-b from-indigo-900 via-stone-700 to-stone-900 pt-4">
+    <div className="fixed inset-0 bg-gradient-to-b from-indigo-900 via-stone-700 to-stone-900 flex items-center justify-center">
       <SearchField />
     </div>
   );
