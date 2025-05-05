212125d6dbacfd94002a88855708db75779eeafe
diboti <korposb@gmail.com>
2025-05-05 23:32:35 +0300

    fixed invalid names

--- src/pages/Browse/SearchField.tsx
+++ src/pages/Browse/SearchField.tsx
@@ -66,9 +66,9 @@ export default function SearchField({
             >
               <path
                 stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 d="M4 12.25V1m0 11.25a2.25 2.25 0 0 0 0 4.5m0-4.5a2.25 2.25 0 0 1 0 4.5M4 19v-2.25m6-13.5V1m0 2.25a2.25 2.25 0 0 0 0 4.5m0-4.5a2.25 2.25 0 0 1 0 4.5M10 19V7.75m6 4.5V1m0 11.25a2.25 2.25 0 1 0 0 4.5 2.25 2.25 0 0 0 0-4.5ZM16 19v-2"
               />
             </svg>
